9483bd1d3d0c3e7e974be82e0ff4919dd77392a2
Lauriita <laurahb123@gmail.com>
2020-02-03 20:28:35 +0100

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+
+    Dim a(4, 4) As Integer
+    Dim i, j As Integer
+    Dim random As New Random
+    Dim suma As Integer = 0
+    Dim cadena As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Randomize()
+        For i = 0 To 3
+            For j = 0 To 3
+                a(i, j) = random.Next(0, 9)
+                cadena = cadena & " " & a(i, j)
+            Next
+            cadena = cadena & vbNewLine
+        Next
+        j = 4
+
+        For i = 0 To 3
+
+            suma = suma + a(i, j)
+
+            j = j - 1
+
+        Next
+
+        cadena = cadena & vbNewLine & suma
+        TextBox1.Text = cadena
+
+    End Sub
+End Class
